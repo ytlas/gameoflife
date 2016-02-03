6bc561e5fd4b3c5cadb74aef9ced035d76710672
RepulsiveChap <joel.ardarve@gmail.com>
2016-02-03 10:12:34 +0100

    start files

---
+++ game.py
@@ -0,0 +1,25 @@
+import pygame
+import sys
+
+
+class Game(object):
+
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode((640, 480))
+        self.clock = pygame.time.Clock()
+
+        while True:
+            self.clock.tick(15)
+            self.update()
+            self.draw()
+
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.window.fill((255, 0, 0))
+        pygame.display.flip()
+
+pacman = Game()
